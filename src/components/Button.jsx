e362ca080af008a3603ecf3cc2e3d34844c4bddf
VovaReka23 <ozil.reka@gmail.com>
2021-01-09 22:25:42 +0200

    server local and filter pizzas

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from 'prop-types';
 import classNames from 'classnames'
 const Button = ({onClick, className, outline, children }) => {
     return (
@@ -11,4 +12,9 @@ const Button = ({onClick, className, outline, children }) => {
         </button>
     )
 }
+Button.propTypes = {
+  onClick: PropTypes.func,
+}
+
 export default Button;
+
